87d993df5b0f67f00195a586dd4b551d8e6cb6fb
hugo maher <maherhugo@gmail.com>
2019-03-04 17:56:30 +0000

    done card ish

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates :name, presence: true
-  validates :email, presence: true
+  # validates :name, presence: true
+  # validates :email, presence: true
   # validates :address presence: true
 
   devise :database_authenticatable, :registerable,
