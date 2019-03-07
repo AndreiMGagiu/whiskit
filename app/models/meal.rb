bda4af83cc4286026c57c5bb321b8612ee419c19
andrei gagiu <gagiumandrei@gmail.com>
2019-03-07 15:33:48 +0000

    done

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,4 +1,5 @@
 class Meal < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
   belongs_to :user
   has_many :orders, dependent: :delete_all
   has_many :meal_dietary_requirements, dependent: :delete_all
