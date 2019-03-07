b6589b18eb412b26b177c297d96f399907e00902
andrei gagiu <gagiumandrei@gmail.com>
2019-03-07 11:51:59 +0000

    done

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,14 +1,13 @@
 class Meal < ApplicationRecord
   belongs_to :user
-  has_many :orders
-  has_many :meal_dietary_requirements
+  has_many :orders, dependent: :delete_all
+  has_many :meal_dietary_requirements, dependent: :delete_all
   has_many :dietary_requirements, through: :meal_dietary_requirements
-
   validates :name, presence: true
   validates :description, presence: true
-#   validates :portions, presence: true
-#   validates :price, presence: true
-#   validates :type_of, presence: true
-#   validates :pick_up_start, presence: true
-#   validates :pick_up_end, presence: true
+  #   validates :portions, presence: true
+  #   validates :price, presence: true
+  #   validates :type_of, presence: true
+  #   validates :pick_up_start, presence: true
+  #   validates :pick_up_end, presence: true
 end
