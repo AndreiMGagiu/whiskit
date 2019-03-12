0b7c2fee8d5828a54b24515490309aabc31c9361
andrei gagiu <gagiumandrei@gmail.com>
2019-03-12 10:51:56 +0000

    done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,5 +2,9 @@ import "bootstrap";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "../plugins/flatpickr"
 import { initMapbox } from '../plugins/init_mapbox';
+import { initAutocomplete } from '../plugins/init_autocomplete'
+import {initForm} from '../plugins/init_form'
 
 initMapbox();
+initAutocomplete();
+initForm();
