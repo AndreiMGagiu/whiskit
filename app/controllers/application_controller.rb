c1560222bbacdb3e87dd5b881c49555d762caaa0
andrei gagiu <gagiumandrei@gmail.com>
2019-03-13 14:59:15 +0000

    ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email address photo])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[email address photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email address photo first_name surname])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[email address photo first_name surname])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_12_113544) do
+ActiveRecord::Schema.define(version: 2019_03_13_141956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,8 @@ ActiveRecord::Schema.define(version: 2019_03_12_113544) do
     t.float "latitude"
     t.float "longitude"
     t.string "photo"
+    t.string "first_name"
+    t.string "surname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
